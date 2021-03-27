ed9d195c7a250b3376bb81cced98afa304eff83e
IgorNiNo <vis_74@mail.ru>
2021-03-28 00:04:53 +0300

    lesson8_commit2

--- app/src/main/java/ru/myproject/android_2_kotlin/app/App.kt
+++ app/src/main/java/ru/myproject/android_2_kotlin/app/App.kt
@@ -1,7 +1,11 @@
 package ru.myproject.android_2_kotlin.app
 
 import android.app.Application
+//import android.os.Handler
+//import android.os.HandlerThread
 import androidx.room.Room
+//import io.reactivex.rxjava3.core.Single
+//import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.myproject.android_2_kotlin.room.HistoryDao
 import ru.myproject.android_2_kotlin.room.HistoryDataBase
 
@@ -37,3 +41,56 @@ class App : Application() {
         }
     }
 }
+
+//        fun getHistoryDao(): HistoryDao {
+//            Single.zip(
+//                //Выполняем запрос на сервер
+//                if (db == null) {
+//                    synchronized(HistoryDataBase::class.java) {
+//                        if (db == null) {
+//                            if (appInstance == null) throw
+//                            IllegalStateException("Application is null while creating DataBase")
+//                            db = Room.databaseBuilder(
+//                                appInstance!!.applicationContext,
+//                                HistoryDataBase::class.java,
+//                                DB_NAME
+//                            ).build()
+//                        }
+//                    }
+//                }
+//
+//            )
+//                .subscribeOn(Schedulers.io())   //выполним запрос в отдельном потоке
+//                .observeOn(AndroidSchedulers.mainThread(db))  //Возвращаем полученные данные в основной поток
+//                .subscribeBy()
+//            return db!!.historyDao()
+//        }
+//    }
+//}
+
+//        fun getHistoryDao(): HistoryDao {
+//            val handlerThreadDb = HandlerThread("HANDLER_THREAD_DB")
+//            handlerThreadDb.start()
+//            val handlerDb = Handler(handlerThreadDb.looper)
+//            Thread {
+//                if (db == null) {
+//                    synchronized(HistoryDataBase::class.java) {
+//                        if (db == null) {
+//                            if (appInstance == null) throw
+//                            IllegalStateException("Application is null while creating DataBase")
+//                            db = Room.databaseBuilder(
+//                                appInstance!!.applicationContext,
+//                                HistoryDataBase::class.java,
+//                                DB_NAME
+//                            ).build()
+//                            handlerDb.post {
+//                                db!!.historyDao()
+//                            }
+//                        }
+//                    }
+//                }
+//            }.start()
+//            return db!!.historyDao()
+//        }
+//    }
+//}
